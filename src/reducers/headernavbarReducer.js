3ab75b02fb3c805849a3a384ba59a206c4835e9f
hugh <hugh90@hotmail.es>
2021-08-20 18:59:16 +0200

    built base structure for account components

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -4,22 +4,8 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    headerLinks: [
-        {
-            title: 'yooo'
-        },
-        {
-            title: 'YOOO'
-        }
-    ],
-    navbarLinks: [
-        {
-            title: 'account'
-        },
-        {
-            title: 'purchases'
-        }
-    ]
+    headerLinks: [],
+    navbarLinks: []
 }
 
 export default function(state = INITIAL_STATE, action) {
