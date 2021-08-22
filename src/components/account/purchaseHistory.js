a1520cff2bba0bcc643cd1b51fbb93bc64770430
hugh <hugh90@hotmail.es>
2021-08-22 18:29:51 +0200

    finished purchases component

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -9,12 +9,13 @@ class PurchaseHistory extends Component {
         return (
             <div className="purchase-history">
                 <PageTitle className="purchase-history__page-title" title="Purchase History"/>
-                <Purchases className="purchase-history__purchases"/>
-                <PurchaesDetail className="purchase-history__detail"/>
-                <div className="purchase-history__border-bottom"></div>
+                <div className='purchase-history__content'>
+                    <Purchases className='purchase-history__purchases'/>
+                    <PurchaseDetail className='purchase-history__detail'/>
+                </div>
+                <div className='purchase-history__border-bottom'></div>
             </div>
         )
     }
 }
-
 export default PurchaseHistory;
